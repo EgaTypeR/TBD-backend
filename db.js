01a6951d58513df693642a3a786fec7245ad408f
EgaTypeR <110679888+EgaTypeR@users.noreply.github.com>
2023-06-30 06:48:09 +0700

    .env excluding

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
 const Pool = require("pg").Pool;
+require("dotenv").config()
+
+
+// const pool = new Pool({
+//   user: process.env.USER,
+//   password: "Tbd0000",
+//   host:"172.173.214.81",
+//   port: 5432,
+//   database:"project-TBD"
+// });
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Tbd0000",
-  host:"172.173.214.81",
-  port: 5432,
-  database:"project-TBD"
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host:process.env.IP_DATABASE,
+  port: process.env.PORT,
+  database:process.env.DATABASE
 });
 
 module.exports = pool;
